cf88f7ab1059af06820a9313780bbfaf9420c789
Austin Williams <austinq.williams@gmail.com>
2019-03-11 16:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
- var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
- 
+
+
  function dwarfRollCall(dwarves){
    var array =[]
    for (var i=0;i<dwarves.length;i++){
@@ -7,6 +7,8 @@
    }
    return array.join("")
  }
+ 
+ //array to string // 
  
 
 function summonCaptainPlanet(planeteerCalls){
@@ -21,24 +23,39 @@ function summonCaptainPlanet(planeteerCalls){
 function longPlaneteerCalls(words){
   for(var i=0;i<words.length;i++){
     if (words[i].length>4){
-      return true
-    }
+      return true }
+      
   }
-  return false
+    return false 
+
 }
  
-var words = ["wind", "fire"]
-longPlaneteerCalls(words)
-
-function findTheCheese (foods) {
+ function findTheCheese (foods) {
   for(var i=0;i<foods.length;i++){
-  if (foods[i]=="cheddar"
+  if (foods[i]==="cheddar"
   ||foods[i]=="gouda"
   ||foods[i]=="camembert"
+  
   ){
     return foods[i]
-  }
+  } 
 }
+// if list of ingredients does not include cheese return the string "no cheese!"
+
 return "no cheese!"
 }
  
+ // words = [ "greg", "apple", "bob"]
+ // should return = [ "bob" ]
+ // "greg".startsWith("b") return false
+ // "bob".startsWith("b") return true
+ function wordswithb(words) {
+   var array =[]
+   for (var i = 0; i < words.length; i++ ) {
+      let word = words[i]
+      if(word.startWith("b")){
+        array.push(words)
+      }
+   }
+   return array
+ }
